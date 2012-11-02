711b661e5257407403ae22a7626cb25f454e178b
StylishFlash <skater_joa@hotmail.com>
2012-11-02 19:52:48 +0100

    OpslaanFilm

--- Bioscoop/src/HQL/OpslaanFilm.java
+++ Bioscoop/src/HQL/OpslaanFilm.java
@@ -2,9 +2,14 @@ package HQL;
 
 import model.Bioscoopholding;
 import model.Film;
+import model.Medewerker;
+import model.Rol;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import persistence.HibernateUtil;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,20 +20,29 @@ import persistence.HibernateUtil;
  */
 public class OpslaanFilm {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
 
         Bioscoopholding bioscoopholding = new Bioscoopholding("Kinepolis", "Gent", "BE001");
         session.saveOrUpdate(bioscoopholding);
 
         Film film  = new Film("The Muppets", "Komedie", "De muppets film", 103, "USA", 1, true);
         film.setBioscoopholding(bioscoopholding);
+        session.saveOrUpdate(film);
 
+        Rol rol = new Rol("Rol van Kermit de kikker", film);
+        session.saveOrUpdate(rol);
 
+        film.addRol(rol);
+        session.saveOrUpdate(rol);
 
-        tx.commit();
+        Medewerker medewerker = new Medewerker("Jan", new SimpleDateFormat("dd/MM/YYY").parse("01/01/1980"), "België");
+        medewerker.addRol(rol);
+        session.save(medewerker);
+
+        transaction.commit();
 
     }
 
